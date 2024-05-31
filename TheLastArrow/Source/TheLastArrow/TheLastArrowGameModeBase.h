3edec87136cb906224f3b80b8c38b67ca0b07feb
algamja <ysk102706@gmail.com>
2024-05-31 15:49:22 +0900

    .

---
+++ TheLastArrow/Source/TheLastArrow/TheLastArrowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheLastArrowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELASTARROW_API ATheLastArrowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
